518054978f8ce05b2ffd99e755f60f821a98964e
Laura Moldovan <laura.duduma@gmail.com>
2021-11-26 17:46:26 +0100

    2

--- error.php
+++ error.php
@@ -5,7 +5,41 @@
         <title>Error</title>
         <?php require_once 'components/boot.php'?>    
     </head>
-    <body>
+    <body class="bg-light">
+    <header>
+        <div class="menu">
+            <ul class="nav">
+                <nav class="navbar navbar-expand-lg navbar-dark">
+                    <div class="container-fluid">
+                        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                            <span class="navbar-toggler-icon"></span>
+                        </button>
+                        <div class="collapse navbar-collapse" id="navbarNav">
+                            <ul class="navbar-nav">
+                                <li class="nav-item">
+                                    <a class="nav-link" href="index.php">
+                                        HOME
+                                    </a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="admin.php">Customize
+                                    </a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="displayAll.php">
+                                        Get API
+                                    </a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </nav>
+            </ul>
+        </div>
+        <div class="m-auto p-5">
+            <p class="h1 text-white text-center title">Mount Everest</p>
+        </div>
+    </header>
         <div class="container">  
             <div class="mt-3 mb-3">
                 <h1>Invalid Request</h1>
