27befb0ee6c096ad7289964c79f5682c7156e907
Laura Moldovan <laura.duduma@gmail.com>
2021-11-27 19:43:34 +0100

    9

--- AJAX/script.js
+++ AJAX/script.js
@@ -9,7 +9,7 @@ function getPlaces() {
             console.log(places.data);
             let output = '<div class="row row-cols-1 row-cols-sm-2 row-cols-md-3">';
             for (let x of places.data) {
-                output += "<div class='col p-3'><div class='card p-0 shadow-lg bg-body rounded all-places'><img class='card-img-top' src='../pictures/places/"+x.picture+"'alt='"+x.name+"'><h4 class='card-header text-center'>"+x.name+"</h4><div class='card-body p-2'><p class='h5 card-text text-center text-success'>" + x.price +" € </p></div><div class='card-footer text-center'><a class='btn btn-small bg-info' href='details.php?id=" + x.id + "'>Show Details</a></div></div></div>";
+                output += "<div class='col p-3'><div class='card p-0 shadow-lg bg-body rounded all-places'><img class='card-img-top' src='../pictures/places/"+x.picture+"'alt='"+x.name+"'><h4 class='card-header text-center'>"+x.name+"</h4><div class='card-body p-2'><p class='h5 card-text text-center text-success'>" + x.price +" € </p></div><div class='card-footer text-center'><a class='btn btn-small bg-info' href='../details.php?id=" + x.id + "'>Show Details</a></div></div></div>";
                 console.log(output);
             }
             output +="</div>";
